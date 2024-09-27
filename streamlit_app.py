c652af6828a62413fbf40e88b458f05eccaad56d
Shivanand <shivanandnaduvin@gmail.com>
2024-09-27 09:51:07 +0000

    modified

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,23 @@
 import streamlit as st
 
-picture = st.camera_input("Take a picture")
+# Custom CSS to make the camera full screen on mobile
+st.markdown(
+    """
+    <style>
+    /* This sets the video and canvas elements to full screen width */
+    @media only screen and (max-width: 600px) {
+        .stCameraInput div div div canvas, .stCameraInput div div div video {
+            width: 100vw !important;
+            height: auto !important;
+        }
+    }
+    </style>
+    """, 
+    unsafe_allow_html=True
+)
 
-if picture:
-    st.image(picture)
+# Camera input to capture an image
+img = st.camera_input("Take a picture")
+
+if img:
+    st.image(img)
