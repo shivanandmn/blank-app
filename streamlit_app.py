725098594a088c4a55b9365b588b794c848299b3
Shivanand <shivanandnaduvin@gmail.com>
2024-09-27 09:47:46 +0000

    gh

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+picture = st.camera_input("Take a picture")
+
+if picture:
+    st.image(picture)
